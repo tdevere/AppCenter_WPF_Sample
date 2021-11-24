19eb44fd55c0f02d6013f47850c5389e87979ac7
tdevere <tdevere@microsoft.com>
2021-11-24 15:23:42 -0600

    Add project files.

---
+++ AppCenter_WPF_Sample/App.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace AppCenter_WPF_Sample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            AppCenterStart();
+
+            base.OnStartup(e);
+        }
+
+        private void AppCenterStart()
+        {            
+            AppCenter.LogLevel = LogLevel.Verbose;
+
+            AppCenter.Start("ee7111d8-fdb8-4888-a98d-82702842f3b0",
+                   typeof(Analytics), typeof(Crashes));
+
+            Crashes.SendingErrorReport += Crashes_SendingErrorReport;
+            Crashes.SentErrorReport += Crashes_SentErrorReport;
+            Crashes.FailedToSendErrorReport += Crashes_FailedToSendErrorReport;
+
+            Crashes.ShouldProcessErrorReport = (ErrorReport report) =>
+            {
+                bool processErrorReport = true;
+
+                IDictionary<String, String> dictionary = report.GetType()
+                  .GetProperties()
+                  .Where(p => p.CanRead && p.PropertyType == typeof(String))
+                  .ToDictionary(p => p.Name, p => (String)p.GetValue(report, null));
+                                
+                Analytics.TrackEvent(Events.ShouldProcessErrorReport.ToString(), dictionary);
+
+                return processErrorReport;
+            };
+
+            Crashes.HasCrashedInLastSessionAsync().ContinueWith((resultTask) =>
+            {
+                if (resultTask.Result)
+                {
+                    Analytics.TrackEvent(Events.CrashedLastSession.ToString());
+                }                
+            });
+        }
+
+        private void Crashes_FailedToSendErrorReport(object sender, FailedToSendErrorReportEventArgs e)
+        {
+            Analytics.TrackEvent(Events.FailedToSendErrorReport.ToString());
+        }
+
+        private void Crashes_SentErrorReport(object sender, SentErrorReportEventArgs e)
+        {
+            Analytics.TrackEvent(Events.SentErrorReport.ToString());
+        }
+
+        private void Crashes_SendingErrorReport(object sender, SendingErrorReportEventArgs e)
+        {
+            Analytics.TrackEvent(Events.SendingErrorReport.ToString());
+        }
+
+        public enum Events
+        {
+            CrashedLastSession,
+            SendingErrorReport,
+            SentErrorReport,
+            FailedToSendErrorReport,
+            ShouldProcessErrorReport
+        }
+    }
+}
---
+++ AppCenter_WPF_Sample/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace AppCenter_WPF_Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnGenerateCrash_Click(object sender, RoutedEventArgs e)
+        {
+            int x = 1;
+            int y = 0;
+            int result = x / y;
+        }
+    }
+}
